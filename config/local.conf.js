d12637e59b34f372168681896bf720f4209ed00d
PFaz540 <paul.farrell@gear4music.com>
2022-10-19 10:38:01 +0100

    added chrome

--- config/local.conf.js
+++ config/local.conf.js
@@ -7,18 +7,35 @@ exports.config = {
         './tests/*.spec.js'
     ],
     maxInstances: 10,
-    capabilities: [{
-        hostname: "localhost",
-        browserName: "firefox",
-        acceptInsecureCerts: true,
-        "moz:firefoxOptions": {
-            "args": [
-                "--headless",
-                "-width=1920",
-                "-height=1080"
-            ]
-        }
-    }],
+    capabilities: [
+        {
+            hostname: "localhost",
+            browserName: "firefox",
+            acceptInsecureCerts: true,
+            "moz:firefoxOptions": {
+                "args": [
+                    "--headless",
+                    "-width=1920",
+                    "-height=1080"
+                ]
+            }
+        },
+        // {
+        //     hostname: process.env.HUB_HOST,
+        //     browserName: "chrome",
+        //     acceptInsecureCerts: true,
+        //     "goog:chromeOptions": {
+        //         "args": [
+        //             "--no-sandbox",
+        //             // "--disable-infobars",
+        //             "--headless",
+        //             // "--disable-gpu",
+        //             "--disable-dev-shm-usage",
+        //             // "--window-size=1920,1080",
+        //         ]
+        //     }
+        // }
+    ],
     logLevel: 'error',
     services: ['selenium-standalone']
 }
