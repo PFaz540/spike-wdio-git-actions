e50de59aa2baba8e54e3b5a6f03fa4d1e063e63a
PFaz540 <paul.farrell@gear4music.com>
2022-10-19 17:08:59 +0100

    docker updates for github actions

--- config/wdio.conf.js
+++ config/wdio.conf.js
@@ -1,15 +1,16 @@
 exports.config = {
     hostname: process.env.HUB_HOST,
     specs: [
-        './tests/*.spec.js'
+        "./tests/*.js"
     ],
-    maxInstances: 10,
+    maxInstances: 1,
     capabilities: [
         {
             hostname: process.env.HUB_HOST,
             browserName: "firefox",
             acceptInsecureCerts: true,
             "moz:firefoxOptions": {
+                "log": { "level": "WARN" },
                 "args": [
                     "--headless",
                     "-width=1920",
@@ -34,30 +35,30 @@ exports.config = {
         // },
     ],
     // Level of logging verbosity: trace | debug | info | warn | error | silent
-    logLevel: 'trace',
+    logLevel: "error",
     bail: 0,
-    baseUrl: 'https://www.gear4music.com',
-    waitforTimeout: 10000,
-    connectionRetryTimeout: 120000,
-    connectionRetryCount: 3,
+    baseUrl: "https://www.gear4music.com",
+    waitforTimeout: 20000,
+    connectionRetryTimeout: 30000,
+    connectionRetryCount: 5,
     services: [],
-    framework: 'mocha',
+    framework: "mocha",
     reporters: [
-        'spec',
-        ['junit', {
-            outputDir: './reports',
+        "spec",
+        ["junit", {
+            outputDir: "./reports",
             outputFileFormat: function (options) { // optional
                 return `wdio-report-${options.cid}.xml`
             }
         }],
-        ['allure', {
+        ["allure", {
             // Defaults to ./allure-results. After a test run is complete, you will find that this directory has been populated with an .xml file for each spec, plus a number of .txt and .png files and other attachments.
-            outputDir: './allure-results',
+            outputDir: "./allure-results",
             // Optional parameter(false by default), shows all the Selenium API Call/Actions in the report.
             disableWebdriverStepsReporting: true,
             // Optional parameter(false by default), do NOT attach screenshots to the reporter.
             // false = Yes, attach
-            // true = Don't attach
+            // true = Don"t attach
             disableWebdriverScreenshotsReporting: false,
             // Optional parameter (false by default), if true this will
             disableMochaHooks: false,
@@ -66,7 +67,7 @@ exports.config = {
         }]
     ],
     mochaOpts: {
-        ui: 'bdd',
+        ui: "bdd",
         timeout: 60000
     },
     afterStep: async ({ error }) => {
