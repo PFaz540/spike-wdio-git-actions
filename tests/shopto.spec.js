69616c1d6a57d6c72a9fc23e6bcf297954246523
PFaz540 <paul.farrell@gear4music.com>
2022-10-21 17:48:02 +0100

    removed slowest test

--- tests/shopto.spec.js
+++
@@ -1,8 +0,0 @@
-describe("ShopTo Tests", () => {
-    it("displays the ShopTo logo", async () => {
-        await browser.url("https://www.shopto.net/");
-        const headerLogo = await $("#header_logo");
-        await browser.saveScreenshot(`./screenshots/shopto.png`);
-        await expect(headerLogo).toBeDisplayed();
-    });
-});
