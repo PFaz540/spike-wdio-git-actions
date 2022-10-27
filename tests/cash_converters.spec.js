42817b1306e41a33ca85e22b58950a2d2dc9d33c
PFaz540 <paul.farrell@gear4music.com>
2022-10-27 19:00:23 +0100

    delete test files for quicker feedback

--- tests/cash_converters.spec.js
+++
@@ -1,8 +0,0 @@
-describe("Cash Converters Tests", () => {
-    it("displays the Cash Converters logo", async () => {
-        await browser.url("https://www.cashconverters.co.uk/");
-        const headerLogo = await $(".header__logo");
-        await browser.saveScreenshot(`./screenshots/cash_converters.png`);
-        await expect(headerLogo).toBeDisplayed();
-    });
-});
