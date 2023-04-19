3914b9f60c847171f755138016b559ffcc1c03a7
PFaz540 <paul.farrell@gear4music.com>
2023-04-19 19:49:36 +0100

    update ci congig

--- config/ci.conf.js
+++ config/ci.conf.js
@@ -29,11 +29,20 @@ exports.config = merge(wdioConf.config, {
     services: [],
     reporters: [
         "spec",
-        ["junit", {
-            outputDir: "./reports",
-            outputFileFormat: function () { // optional
-                return `report-${Date.now()}.xml`
-            }
+        ["allure", {
+            // Defaults to ./allure-results. 
+            // After a test run is complete, you will find that this directory has been populated with an .xml file for each spec, plus a number of .txt and .png files and other attachments.
+            outputDir: "./allure-results",
+            // Optional parameter(false by default), shows all the Selenium API Call/Actions in the report.
+            disableWebdriverStepsReporting: true,
+            // Optional parameter(false by default), do NOT attach screenshots to the reporter.
+            // false = Yes, attach
+            // true = Don"t attach
+            disableWebdriverScreenshotsReporting: false,
+            // Optional parameter (false by default), if true this will
+            disableMochaHooks: false,
+            // Optional parameter(false by default), set to true in order to attach console logs from step to the reporter.
+            addConsoleLogs: true
         }]
     ],
     // Waits 20 seconds for page load or elements to appear before failing
